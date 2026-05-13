83970b8a1c96541cc78ea564311be0dc75176ffb
VI-Alumno <ma.beas@duocuc.cl>
2026-05-13 09:18:44 -0400

    Agrege los log para cada metodo (quizas se agregen mas)

--- src/main/java/cl/Proyecto/Prevision/Service/PrevisionService.java
+++ src/main/java/cl/Proyecto/Prevision/Service/PrevisionService.java
@@ -5,12 +5,14 @@ import cl.Proyecto.Prevision.dto.PrevisionResponseDTO;
 import cl.Proyecto.Prevision.modelo.Prevision;
 import cl.Proyecto.Prevision.repository.PrevisionRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PrevisionService {
@@ -25,15 +27,18 @@ public class PrevisionService {
     }
 
     public List<PrevisionResponseDTO> obtenerTodos(){
+        log.info("Consultando TODAS las previsiones");
         return previsionRepository.findAll().stream()
                 .map(this::mapToDTO).collect(Collectors.toList());
     }
 
     public Optional<PrevisionResponseDTO> obtenerPorId(Long id){
+        log.info("Consultando la prevision con el ID: " + id);
         return previsionRepository.findById(id).map(this::mapToDTO);
     }
 
     public PrevisionResponseDTO guardar(PrevisionRequestDTO dto){
+        log.info("Creando Prevision");
         Prevision prevision = new Prevision(
                 null,
                 dto.getNombre()
@@ -42,6 +47,7 @@ public class PrevisionService {
     }
 
     public Optional<PrevisionResponseDTO> actualizar(Long id, PrevisionRequestDTO dto){
+        log.info("Actualizando Prevision con ID: " + id);
         return previsionRepository.findById(id)
                 .map(existente -> {existente.setNombre(dto.getNombre());
                 return mapToDTO(previsionRepository.save(existente));
@@ -50,5 +56,6 @@ public class PrevisionService {
 
     public void eliminar(Long id){
         previsionRepository.deleteById(id);
+        log.info("Eliminado Prevision con ID: "+ id);
     }
 }
