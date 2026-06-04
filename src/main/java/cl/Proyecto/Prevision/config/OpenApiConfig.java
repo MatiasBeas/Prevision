3606953fc6bcc25f98a4a5625c349b1c45a16411
Matias <ma.beas@duocuc.cl>
2026-06-04 12:12:49 -0400

    intento de swagger

---
+++ src/main/java/cl/Proyecto/Prevision/config/OpenApiConfig.java
@@ -0,0 +1,8 @@
+package cl.Proyecto.Prevision.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+}
--- src/main/java/cl/Proyecto/Prevision/config/SecurityConfig.java
+++ src/main/java/cl/Proyecto/Prevision/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain filtroPro(HttpSecurity http) throws Exception {
         http
