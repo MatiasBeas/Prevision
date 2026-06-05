10400e958a3efdefd422c29f1020b160a8c969c8
Matias <ma.beas@duocuc.cl>
2026-06-05 12:16:59 -0400

    Cambios en los comentarios para swagger, agregar dependencias para eurecka y cambio de version

--- src/main/java/cl/Proyecto/Prevision/PrevisionApplication.java
+++ src/main/java/cl/Proyecto/Prevision/PrevisionApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrevisionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrevisionApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(PrevisionApplication.class, args);
 	}
-
 }
